f7c5bbec96747cb49c73ca45e89d7cb97d8fbd8f
piercemacleod0-hub <piercemacleod0@gmail.com>
2026-07-12 19:16:46 +0800

    发布 Windows 手机实时共享 v2.1.0 (#4)
    Co-authored-by: piercemacleod0-hub <295138738+piercemacleod0-hub@users.noreply.github.com>

--- src/preload.js
+++ src/preload.js
@@ -5,6 +5,10 @@ contextBridge.exposeInMainWorld('pulseboard', {
   getLatest: () => ipcRenderer.invoke('latest:get'),
   openData: () => ipcRenderer.invoke('data:open'),
   getAppInfo: () => ipcRenderer.invoke('app:info'),
+  getSharing: () => ipcRenderer.invoke('sharing:get'),
+  updateSharing: (settings) => ipcRenderer.invoke('sharing:update', settings),
+  copyText: (text) => ipcRenderer.invoke('clipboard:write', text),
   onSample: (callback) => ipcRenderer.on('sample', (_event, sample) => callback(sample)),
-  onError: (callback) => ipcRenderer.on('collector-error', (_event, message) => callback(message))
+  onError: (callback) => ipcRenderer.on('collector-error', (_event, message) => callback(message)),
+  onSharingError: (callback) => ipcRenderer.on('sharing-error', (_event, message) => callback(message))
 });
